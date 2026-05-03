eec5431912c462febbb880e2253a73cad2607752
Nadhifah Eka C <nadhifaerkacs@gmail.com>
2026-05-03 21:45:48 +0700

    Menambahkan API Film

---
+++ Controllers/FilmController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TP_MODUL10_103022400028.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmController : ControllerBase
+    {
+        // daftar film statis
+        public static List<Film> films = new List<Film>
+        {
+            new Film("Inception", "Christopher Nolan", "2010", "Sci-Fi", "9.0"),
+            new Film("Interstellar", "Christopher Nolan", "2014", "Sci-Fi", "8.7"),
+            new Film("Parasite", "Bong Joon-ho", "2019", "Thriller", "8.6")
+        };
+        // mengembalikan semua film
+        [HttpGet]
+        public IEnumerable<Film> Get()
+        {
+            return films;
+        }
+        // mengembalikan film berdasarkan index
+        [HttpGet("{index}")]
+        public Film Get(int index)
+        {
+            return films[index];
+        }
+        // menambahkan film baru
+        [HttpPost]
+        public void Post([FromBody] Film film)
+        {
+            films.Add(film);
+        }
+        // mengupdate or hpus film berdasarkan index
+        [HttpDelete("{index}")]
+        public void Delete(int index)
+        {
+            films.RemoveAt(index);
+        }
+    }
+}
---
+++ Film.cs
@@ -0,0 +1,20 @@
+﻿namespace TP_MODUL10_103022400028
+{
+    public class Film
+    {
+        public string Judul { get; set; }
+        public string Sutradara { get; set; }
+        public string Tahun { get; set; }
+        public string Genre { get; set; }
+        public string Rating { get; set; }
+
+        public Film(string judul, string sutradara, string tahun, string genre, string rating)
+        {
+            Judul = judul;
+            Sutradara = sutradara;
+            Tahun = tahun;
+            Genre = genre;
+            Rating = rating;
+        }
+    }
+}
